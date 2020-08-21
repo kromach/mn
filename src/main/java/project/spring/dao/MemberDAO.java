cbae4d4accd1f4d0863f7ebd7ee2da056d82054f
yskim-web <abcm556@naver.com>
2020-08-21 12:04:03 +0900

    #

--- src/main/java/project/spring/dao/MemberDAO.java
+++ src/main/java/project/spring/dao/MemberDAO.java
@@ -1,9 +1,18 @@
 package project.spring.dao;
 import java.sql.SQLException;
+import java.util.List;
+
 import project.spring.vo.MemberDTO;
 
 public interface MemberDAO {
+	//회원가입
 	public void insertMember(MemberDTO dto) throws SQLException;
+	//아이디 비밀번호확인
 	public int idPwCheck(MemberDTO dto) throws SQLException;
+	//한명의 데이터 조회
 	public MemberDTO selectMember(String id) throws SQLException;
+	//전체 회원 데이터 조회
+	public List selectAll() throws SQLException;
+	//회원 데이터 수정
+	public void UpdateMember(MemberDTO dto) throws SQLException;
 }
