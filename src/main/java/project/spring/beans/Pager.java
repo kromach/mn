ff5d0c0f3550742ba3ca38f6b26e1a716493e3f4
yskim-web <abcm556@naver.com>
2020-08-25 11:24:56 +0900

    #

--- src/main/java/project/spring/beans/Pager.java
+++ src/main/java/project/spring/beans/Pager.java
@@ -1,23 +1,45 @@
 package project.spring.beans;
+
 import project.spring.vo.PageVO;
+
 public class Pager {
-	private int pageSize = 10;//보여줄 페이지 셋팅
-	private int currPage;//현재 페이지 정보 셋팅
+	//dao.getArticle에서 사용되는 값
 	private int startRow;//현재 페이지 시작
 	private int endRow;//끝 게시물 번호
+	//기본값
+	private int pageSize = 10;//보여줄 페이지 셋팅
+	private int currPage;//현재 페이지 정보 셋팅
 	private int count;//DB에 있는 게시글의 총개수를 담을 변수
 	private int number;//게시판 상의 글번호 뿌려줄 변수
+	//View Page에서 계산되는값
+	private int pageCount = 0;
+	private int pageBlock = 10;
+	private int startPage = 0;
+	private int endPage = 0;
 	public Pager(){
 	}
-	public PageVO Pager(String pageNum) {
+	public PageVO pager(String pageNum,int Count) {
+		
+		//기본셋팅 로직
 		currPage = Integer.parseInt(pageNum);
-		this.currPage = currPage;
 		startRow = (currPage-1)*pageSize +1;
 		endRow = currPage*pageSize;
-		
-		PageVO pager = new PageVO(pageSize,
-				currPage, startRow, endRow, count,
-				number);
+		count = 0;
+		number = 0;
+		//계산되는 로직
+		number = count -(currPage-1)*pageSize;
+		if(count>0) {
+			pageCount = (count/pageSize) +(count%pageSize == 0 ?0 :1);
+		}
+		startPage = ((currPage-1)/pageBlock)*pageBlock+1;
+		endPage = startPage+pageBlock-1;
+		if(endPage>pageCount) {
+			endPage = pageCount;
+		}
+		PageVO pager = new PageVO(startRow,
+				endRow, pageSize, currPage, count,
+				number, pageCount, pageBlock,
+				startPage, endPage);
 		return pager;
 	}
 }
--- src/main/java/project/spring/vo/PageVO.java
+++ src/main/java/project/spring/vo/PageVO.java
@@ -1,24 +1,19 @@
 package project.spring.vo;
 
 public class PageVO {
-	private int pageSize = 10;//보여줄 페이지 셋팅
-	private int currPage;//현재 페이지 정보 셋팅
+	//dao.getArticle에서 사용되는 값
 	private int startRow;//현재 페이지 시작
 	private int endRow;//끝 게시물 번호
+	//기본값
+	private int pageSize = 10;//보여줄 페이지 셋팅
+	private int currPage;//현재 페이지 정보 셋팅
 	private int count;//DB에 있는 게시글의 총개수를 담을 변수
 	private int number;//게시판 상의 글번호 뿌려줄 변수
-	public int getPageSize() {
-		return pageSize;
-	}
-	public void setPageSize(int pageSize) {
-		this.pageSize = pageSize;
-	}
-	public int getCurrPage() {
-		return currPage;
-	}
-	public void setCurrPage(int currPage) {
-		this.currPage = currPage;
-	}
+	//View Page에서 계산되는값
+	private int pageCount = 0;
+	private int pageBlock = 10;
+	private int startPage = 0;
+	private int endPage = 0;
 	public int getStartRow() {
 		return startRow;
 	}
@@ -30,6 +25,18 @@ public class PageVO {
 	}
 	public void setEndRow(int endRow) {
 		this.endRow = endRow;
+	}
+	public int getPageSize() {
+		return pageSize;
+	}
+	public void setPageSize(int pageSize) {
+		this.pageSize = pageSize;
+	}
+	public int getCurrPage() {
+		return currPage;
+	}
+	public void setCurrPage(int currPage) {
+		this.currPage = currPage;
 	}
 	public int getCount() {
 		return count;
@@ -43,24 +50,57 @@ public class PageVO {
 	public void setNumber(int number) {
 		this.number = number;
 	}
-	public PageVO(int pageSize, int currPage,
-			int startRow, int endRow, int count,
-			int number) {
-		super();
-		this.pageSize = pageSize;
-		this.currPage = currPage;
-		this.startRow = startRow;
-		this.endRow = endRow;
-		this.count = count;
-		this.number = number;
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	public int getPageBlock() {
+		return pageBlock;
+	}
+	public void setPageBlock(int pageBlock) {
+		this.pageBlock = pageBlock;
+	}
+	public int getStartPage() {
+		return startPage;
+	}
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
 	}
 	@Override
 	public String toString() {
-		return "PageVO [pageSize=" + pageSize
-				+ ", currPage=" + currPage
-				+ ", startRow=" + startRow
+		return "PageVO [startRow=" + startRow
 				+ ", endRow=" + endRow
+				+ ", pageSize=" + pageSize
+				+ ", currPage=" + currPage
 				+ ", count=" + count + ", number="
-				+ number + "]";
+				+ number + ", pageCount="
+				+ pageCount + ", pageBlock="
+				+ pageBlock + ", startPage="
+				+ startPage + ", endPage="
+				+ endPage + "]";
+	}
+	public PageVO(int startRow, int endRow,
+			int pageSize, int currPage, int count,
+			int number, int pageCount,
+			int pageBlock, int startPage,
+			int endPage) {
+		this.startRow = startRow;
+		this.endRow = endRow;
+		this.pageSize = pageSize;
+		this.currPage = currPage;
+		this.count = count;
+		this.number = number;
+		this.pageCount = pageCount;
+		this.pageBlock = pageBlock;
+		this.startPage = startPage;
+		this.endPage = endPage;
 	}
 }
