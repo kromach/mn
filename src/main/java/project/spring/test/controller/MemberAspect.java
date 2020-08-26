67b81ce6267d72462f3876223479722a08338e38
yskim-web <abcm556@naver.com>
2020-08-26 15:20:01 +0900

    #

--- src/main/java/project/spring/test/controller/MemberAspect.java
+++ src/main/java/project/spring/test/controller/MemberAspect.java
@@ -1,4 +1,4 @@
-package project.spring.aop;
+package project.spring.test.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -9,23 +9,21 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Aspect
+@Component
 public class MemberAspect {
 	private static final Logger logger = LoggerFactory.getLogger(MemberAspect.class);
 	@Around("execution(* project.spring.test.controller.*Controller.*(..))")
 	public Object around(ProceedingJoinPoint j)
 			throws Throwable {
 
-		logger.info("beforeMethod()==");
-		System.out.println(
-				"memberAspect.beforeMethod");
+		System.out.println("===================memberAspect.beforeMethod");
 		Object result = j.proceed();
-		logger.info("afterMethod()==");
-		System.out.println(
-				"memberAspect.afterMethod");
+		System.out.println("===================memberAspect.afterMethod");
 		return result;
 	}
 }
