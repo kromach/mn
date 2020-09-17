fc32bc1ab27a4f96187a36510fce8c50f3573701
yskim-web <abcm556@naver.com>
2020-09-17 14:20:43 +0900

    #

--- src/main/java/project/spring/beans/CsvServiceBean.java
+++ src/main/java/project/spring/beans/CsvServiceBean.java
@@ -1,6 +1,35 @@
 package project.spring.beans;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import au.com.bytecode.opencsv.CSVWriter;
+
+@Service
 public class CsvServiceBean {
-	//1. 파일 쓰기
-	//2. 파일 읽기
+
+	// 1. 파일 쓰기
+	public void writeCSV(List<String[]> data,
+			String fileName) {
+		CSVWriter csvWriter = null;
+		try {
+			csvWriter = new CSVWriter(new FileWriter(fileName),',','"');
+			Iterator<String[]> it =  data.iterator();
+			while(it.hasNext()) {
+				String[] s = (String[])it.next();
+				csvWriter.writeNext(s);
+			}
+		} catch (Exception e) {e.printStackTrace();} 
+		finally {if (csvWriter != null)try {csvWriter.close();} catch (IOException e) {e.printStackTrace();}
+		}
+	}
+
+	// 2. 파일 읽기
+	public void readCSV() {
+
+	}
 }
--- src/main/java/project/spring/member/controller/CSVController.java
+++ src/main/java/project/spring/member/controller/CSVController.java
@@ -1,19 +1,38 @@
 package project.spring.member.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import project.spring.beans.CsvServiceBean;
 
 @Controller
 @RequestMapping("/csv/")
 public class CSVController {
 	
+	@Autowired
+	private CsvServiceBean csvService = null;
+	
 	@RequestMapping("writerCsv")
-	public String writeCSV(){
+	public String writeCSV(HttpServletRequest request){
+//		csvService
+		List<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"abc","def","ghi","jkl"});
+		data.add(new String[] {"1","23","456","7890"});
+		String savePath = request.getRealPath("resources\\csv");
+		savePath += "\\testscv.csv";
+		csvService.writeCSV(data,savePath);
 		return "/util/csvTest";
 	}
 	
 	@RequestMapping("readCsv")
 	public String readCSV(){
+		csvService.readCSV();
 		return "/util/csvTest";
 	}
 }
