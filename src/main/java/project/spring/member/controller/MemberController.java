d92ff5172720d813dcf239c7ff851f24d46c7635
yskim-web <abcm556@naver.com>
2020-09-28 11:54:09 +0900

    #

---
+++ src/main/java/project/spring/member/controller/MemberController.java
@@ -0,0 +1,25 @@
+package project.spring.member.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	
+	@RequestMapping("/signup")
+	public String signUpMember(HttpServletRequest reqest) {
+		String returnUrl = "main";
+		HttpSession session =  reqest.getSession();
+		if(session==null) {
+			returnUrl = "signupForm";
+		}else {
+			returnUrl = "main";
+			System.out.println("로그인이 된 상태입니다.");
+		}
+		return "returnUrl";
+	}
+}
