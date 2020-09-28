a5a60204b9897f995258d3e4f477f984f0c25003
lyg0617@naver.com <lyg0617@naver.com>
2020-09-28 16:40:22 +0900

    #

---
+++ src/main/java/project/spring/admin/service/AdminService.java
@@ -0,0 +1,13 @@
+package project.spring.admin.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import project.spring.beans.CommonInterface;
+
+public interface AdminService extends CommonInterface{
+
+	public List memberLIst(int start, int end) throws SQLException;
+	
+	public int memberCount() throws SQLException;
+}
