db224f16235074d979ac9cd809590811696e75f2
bbqjsk@naver.com <bbqjsk@naver.com>
2020-09-28 17:43:08 +0900

    #

---
+++ src/main/java/project/spring/product/controller/ProductController.java
@@ -0,0 +1,17 @@
+package project.spring.product.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/product/")
+public class ProductController {
+	
+	@RequestMapping("productList")
+		public String productList() {
+		
+		return "/product/productList";
+	}
+
+}
