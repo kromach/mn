ed926839166b639968d24cdd9f883f67e09e84e4
qndbslekd000@gmail.com <qndbslekd000@gmail.com>
2020-09-28 13:49:16 +0900

    #boo62 directory create

--- src/main/java/project/spring/myAct/controller/MyActController.java
+++ src/main/java/project/spring/myAct/controller/MyActController.java
@@ -7,31 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/myAct")
 public class MyActController {
-
-	@RequestMapping(value = "/", method = RequestMethod.POST)
+	@RequestMapping
 	public String index() {
 		System.out.println("MyActIndex");
 		return "/myAct/index.mn";
 	}
-	
-	@RequestMapping(value = "/likeDrink", method = RequestMethod.POST)
-	public String likeDrink() {
-		return "/myAct/likeDrink.mn";
-	}
-	
-	@RequestMapping(value = "/likeArticle", method = RequestMethod.POST)
-	public String likeArticle() {
-		return "/myAct/likeArticle.mn";
-	}
-	
-	@RequestMapping(value = "/title", method = RequestMethod.POST)
-	public String title() {
-		return "/myAct/title.mn";
-	}
-	
-	@RequestMapping(value = "/attend", method = RequestMethod.POST)
-	public String attend() {
-		return "/myAct/attend.mn";
-	}	
-
 }
