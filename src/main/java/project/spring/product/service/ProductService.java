434f13e593a9226c11253b81940474ea86b35ce1
bbqjsk@naver.com <bbqjsk@naver.com>
2020-09-29 16:36:01 +0900

    09.29

--- src/main/java/project/spring/product/service/ProductService.java
+++ src/main/java/project/spring/product/service/ProductService.java
@@ -1,4 +1,4 @@
-package project.spring.product.dao;
+package project.spring.product.service;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -6,9 +6,7 @@ import java.util.List;
 import project.spring.beans.CommonInterface;
 import project.spring.product.vo.ProductVo;
 
-public interface ProductDAO extends CommonInterface {
-	
-	
+public interface ProductService extends CommonInterface {
 	
 	// 판매할 상품목록수 불러오기
 	public int getproductcount() throws SQLException; 
@@ -16,8 +14,5 @@ public interface ProductDAO extends CommonInterface {
 	public List getproduct() throws SQLException;
 	// 게시글 한개 정보 가져오기
 	public ProductVo getproductinfo() throws SQLException;
-	
-	
-	
 
 }
