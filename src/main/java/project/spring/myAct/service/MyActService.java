bacc1c5cf9a0b646478d400e5c79ac3bf7d58b88
qndbslekd000@gmail.com <qndbslekd000@gmail.com>
2020-10-08 17:12:48 +0900

    칭호 없애고싶다

--- src/main/java/project/spring/myAct/service/MyActService.java
+++ src/main/java/project/spring/myAct/service/MyActService.java
@@ -3,15 +3,16 @@ package project.spring.myAct.service;
 import java.util.List;
 
 import project.spring.beans.CommonInterface;
+import project.spring.myAct.vo.TitleListDTO;
 
 public interface MyActService extends CommonInterface{
 	public int myArticleCount(String memId);
 	public List getMyArticle(String memId);
 	public int likeArticleCount(String memId);
 	public List myLikeArticle(String memId);
-	public int likeDrinkCount(String memId);
-	public List myLikeDrink(String memId);
 	public List getMyTitle(String memId);
+	public List updateTitle(String memId);
 	public List getAllTitle();
-	//public int updateTitle();
+	public List myLikeDrink(String memId);
+	public List myLikeProduct(String memId);
 }
