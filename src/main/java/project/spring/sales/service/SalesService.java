f6543102f60c58440126ebd54479b2364e919510
qndbslekd000@gmail.com <qndbslekd000@gmail.com>
2020-10-19 15:37:32 +0900

    #sales/insert

--- src/main/java/project/spring/sales/service/SalesService.java
+++ src/main/java/project/spring/sales/service/SalesService.java
@@ -1,8 +1,10 @@
 package project.spring.sales.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import project.spring.beans.CommonInterface;
+import project.spring.sales.vo.ProductInfoDTO;
 
 public interface SalesService extends CommonInterface{
 
@@ -10,5 +12,7 @@ public interface SalesService extends CommonInterface{
 	public List productorList(String memId, int startRow, int endRow);
 	public String userIdCh(String memId);
 	public List productModify(String pageNum);
+	public List getCategory();
+	public String makeprCode(ProductInfoDTO productDTO);
 
 }
